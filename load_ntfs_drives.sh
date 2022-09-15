a368eef520c2d94bf66a970d4c32c9cd00d9f471
Mikey <mikey.x88@gmail.com>
2022-09-15 09:50:28 -0700

    Refactor NTFS mount script
    - Add check to see if mount point already exists
    - Format the code
    - Add comments

--- load_ntfs_drives.sh
+++ load_ntfs_drives.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env ash
 
+# Do not include the above shebang (#!...) when you paste this script in the
+# Local Startup section (under System -> Startup -> Local Startup tab)
+
+
 # This is a script that loads NTFS drives in OpenWRT
 # Paste or call this script in the Local Startup section
 # NOTES:
@@ -11,12 +15,26 @@ NTFS_DRIVES=$(lsblk -f -J | jq -r '.blockdevices | .[] | select(.children) | .ch
 for i in $NTFS_DRIVES; do
     drive=$(echo $i | cut -d ',' -f 1);
     label=$(echo $i | cut -d ',' -f 2);
+    
+    # Create mount point; if drive has a label, use that as the mnt name
+    mount_name='';
     if [[ -z $label ]]; then
-        label=$drive;
+        mount_name=$drive;
     fi
-    mnt_point=/mnt/$label;
+    mnt_point=/mnt/$mount_name;
+    
+    # Handle case where multiple drives have the same label/mount point
+    i=1;
+    while [[ -e $mnt_point ]]; do
+        mnt_point=/mnt/$mount_name_$i;
+        i=i+1;
+    done
+
+    # Create the mount directory
     if [[ ! -e $mnt_point ]]; then
         mkdir -p $mnt_point;
     fi
+
+    # Finally, mount the drive
     ntfs-3g /dev/$drive $mnt_point -o rw,noatime,uid=1001,gid=1001;
 done
