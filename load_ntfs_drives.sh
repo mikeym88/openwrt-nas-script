6d0052cf1d84a6e796e843977ba444b1b1e968e8
Mike Martinescu <mikey.x88@gmail.com>
2022-09-15 21:57:58 -0700

    Refactor NTFS loading script

--- load_ntfs_drives.sh
+++ load_ntfs_drives.sh
@@ -10,7 +10,8 @@
 #   - must have the lsblk and jq packages installed
 #   - must specify user (see "uid=1001,gid=1001" below)
 
-NTFS_DRIVES=$(lsblk -f -J | jq -r '.blockdevices | .[] | select(.children) | .children[] | select(.fstype=="ntfs") | (.name + "," + .label)');
+# Using JSON to select all NTFS drives that have not been mounted yet
+NTFS_DRIVES=$(lsblk -f -J | jq -r '.blockdevices | .[] | select(.children) | .children[] | select(.fstype=="ntfs") | select(.mountpoints[] == null) | (.name + "," + .label)');
 
 for i in $NTFS_DRIVES; do
     drive=$(echo $i | cut -d ',' -f 1);
@@ -20,14 +21,17 @@ for i in $NTFS_DRIVES; do
     mount_name='';
     if [[ -z $label ]]; then
         mount_name=$drive;
+    else
+        mount_name=$label;
     fi
     mnt_point=/mnt/$mount_name;
     
     # Handle case where multiple drives have the same label/mount point
     i=1;
-    while [[ -e $mnt_point ]]; do
-        mnt_point=/mnt/$mount_name_$i;
-        i=i+1;
+    while (mountpoint -q -- "$mnt_point"); do
+        mnt_point=/mnt/"$mount_name$i";
+        echo $mnt_point;
+        i=$((i+1));
     done
 
     # Create the mount directory
@@ -36,5 +40,6 @@ for i in $NTFS_DRIVES; do
     fi
 
     # Finally, mount the drive
+    echo "Mounting /dev/$drive $mnt_point";
     ntfs-3g /dev/$drive $mnt_point -o rw,noatime,uid=1001,gid=1001;
 done
